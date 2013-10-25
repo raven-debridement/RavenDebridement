71b8586e9a4bde9370a144fcbd30b74378717952
Jeff Mahler <jmahler@rll4.(none)>
2013-10-25 12:55:19 -0700

    Added the ability to set the arm from the command line

--- tests/GridMove.py
+++ tests/GridMove.py
@@ -1,14 +1,18 @@
+#!/usr/bin/env python
+
 import roslib
 import rospy
 roslib.load_manifest('RavenDebridement')
+import random
 import tfx
+import argparse
 
 from raven_2_trajectory.raven_arm import RavenArm
 
 import IPython
 
 class GridMove():
-    def __init__(self, armName, x=.03, y=.05, z=.004):
+    def __init__(self, armName, x=.03, y=.05, z=.004, testAngles = False):
         self.ravenArm = RavenArm(armName)
         
         if armName == 'R':
@@ -16,13 +20,30 @@ class GridMove():
         else:
             self.homePose = tfx.pose([-.03,-.02,-.135],tfx.tb_angles(-90,90,0))
         
-        self.x_levels = 5
-        self.y_levels = 5
+        self.x_levels = 3
+        self.y_levels = 3
         self.z_levels = 5
         
         self.x = x
         self.y = y
         self.z = z
+        
+        # generate all angle combinations in order to sample the rotations as well
+        self.angles = []
+        
+        self.angles.append(tfx.tb_angles(-90,90,0))
+        
+        if testAngles:
+            self.angles.append(tfx.tb_angles(-90,75,0))
+            self.angles.append(tfx.tb_angles(-90,105,0))
+            
+            self.angles.append(tfx.tb_angles(-75,90,0))
+            self.angles.append(tfx.tb_angles(-75,75,0))
+            self.angles.append(tfx.tb_angles(-75,105,0))
+            
+            self.angles.append(tfx.tb_angles(-105,90,0))
+            self.angles.append(tfx.tb_angles(-105,75,0))
+            self.angles.append(tfx.tb_angles(-105,105,0))
         
         self.grid = []
         self.grid += [self.homePose + [float(i)*(x/self.x_levels),0,0] for i in xrange(self.x_levels)]
@@ -48,22 +69,51 @@ class GridMove():
         
     def getGrid(self, homePose):
         grid = []
-        grid += [homePose + [float(i)*(self.x/self.x_levels),0,0] for i in xrange(self.x_levels)]
-        grid += [homePose + [self.x, float(i)*(-self.y/self.y_levels),0] for i in xrange(self.x_levels)]
-        grid += [homePose + [self.x-float(i)*(self.x/self.x_levels),-self.y,0] for i in xrange(self.x_levels)]
-        grid += [homePose + [0, -self.y+float(i)*(self.y/self.y_levels),0] for i in xrange(self.x_levels)]
+        
+        for i in xrange(self.x_levels):
+            for j in xrange(len(self.angles)):
+                pose = homePose + [float(i)*(self.x/self.x_levels),0,0]
+                pose.tb_angles = self.angles[j]
+                grid += [pose]
+        
+        for i in xrange(self.x_levels):
+            for j in xrange(len(self.angles)):
+                pose = homePose + [self.x, float(i)*(-self.y/self.y_levels),0]
+                pose.tb_angles = self.angles[j]
+                grid += [pose]
+                
+        for i in xrange(self.x_levels):
+            for j in xrange(len(self.angles)):
+                pose = homePose + [self.x-float(i)*(self.x/self.x_levels),-self.y,0]
+                pose.tb_angles = self.angles[j]
+                grid += [pose]
+                
+        for i in xrange(self.x_levels):
+            for j in xrange(len(self.angles)):
+                pose = homePose + [0, -self.y+float(i)*(self.y/self.y_levels),0]
+                pose.tb_angles = self.angles[j]
+                grid += [pose]
         
         return grid
         
     def moveByGrid(self, homePose):
-        grid = self.getGrid(homePose)
+        grid = []
+        for _ in xrange(self.z_levels):
+            homePose.position.z -= self.z
+            grid += self.getGrid(homePose)
         
         print 'moveByGrid'
+        print "GRID", len(grid)
         
+        random.seed(1000)
+        random.shuffle(grid)
+        homePose = grid[0]
         self.ravenArm.goToGripperPose(homePose)
         for pose in grid:
             print pose
             rospy.sleep(3)
+            if rospy.is_shutdown():
+                return
             self.ravenArm.goToGripperPose(pose)
         
     def run(self):
@@ -73,21 +123,23 @@ class GridMove():
         raw_input()
         
         homePose = self.homePose
-        
-        while not rospy.is_shutdown():
-            for _ in xrange(self.z_levels):
-                self.moveByGrid(homePose)
-                homePose.position.z -= self.z
+        self.moveByGrid(homePose)
                 
-                if rospy.is_shutdown():
-                    return
-            
+        if rospy.is_shutdown():
+            return
                 
         self.ravenArm.stop()
         
 if __name__ == '__main__':
     rospy.init_node('GridMove', anonymous=True)
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('arm',nargs='?',default='L')
+    args = parser.parse_args(rospy.myargv()[1:])
+    arm_side = args.arm
+    del args.arm
+    
     rospy.sleep(2)
-    gm = GridMove('R')
+    gm = GridMove(arm_side)
     gm.run()        
         
